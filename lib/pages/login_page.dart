b82d5eb738ead84e38195caca96c61e3c9ddbd53
farrellh1 <farrell.hauzan@gmail.com>
2022-08-16 16:09:47 +0700

    slicing login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,7 +7,9 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 24),
+        width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -16,6 +18,7 @@ class LoginPage extends StatelessWidget {
               width: 145,
               height: 62,
             ),
+            const SizedBox(height: 64),
             Text(
               'Masuk',
               style: GoogleFonts.nunitoSans(
@@ -23,6 +26,85 @@ class LoginPage extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
+            const SizedBox(height: 16),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Username',
+                style: GoogleFonts.nunitoSans(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Color(0xffF1F2F4),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Color(0xffF1F2F4),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                hintText: 'Masukan username',
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Password',
+                style: GoogleFonts.nunitoSans(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Color(0xffF1F2F4),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Color(0xffF1F2F4),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                hintText: 'Masukan password',
+              ),
+            ),
+            const SizedBox(height: 16),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: const Color(0xffF34141),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Masuk Akun',
+                  style: GoogleFonts.nunitoSans(
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
